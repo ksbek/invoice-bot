d09c37b3fd7c37ce6558356054ec8acafc059c27
ksbek <ksbek112@yandex.com>
2016-05-11 20:06:08 +0300

    fix template path

--- modules/notifications/server/mailer/notifications.server.mailer.js
+++ modules/notifications/server/mailer/notifications.server.mailer.js
@@ -5,8 +5,8 @@ module.exports = function (config, invoice, EmailTemplate) {
   var sendgrid = require("sendgrid")(config.sendgrid.apiKey);
   var email = new sendgrid.Email();
 
-  var templateDir = require('path').resolve('modules/notifications/server/views/mail_templates');
-
+  var templateDir = require('path').resolve('./modules/notifications/server/views/mail_templates');
+  console.log(templateDir);
   var newsletter = new EmailTemplate(templateDir);
 
   newsletter.render({ invoice: invoice }, function (err, result) {
